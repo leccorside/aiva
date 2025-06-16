a2327c946429e69319919c05546ca7e93f2872ff
Johnathan <leccorside@gmail.com>
2025-06-16 17:44:38 -0300

    Ajuste de limite de produtos

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -2,18 +2,30 @@ import { useQuery } from "@tanstack/react-query";
 import { getProducts } from "@/services/products";
 import type { ProductType } from "@/types/product";
 
-export function useProducts(page: number) {
-  const limit = 10;
+export function useProducts({
+  page = 1,
+  limit = 10,
+}: {
+  page?: number;
+  limit?: number;
+}) {
   const offset = (page - 1) * limit;
 
-  const productsQuery = useQuery<ProductType[], Error>({
+  const { data, error, isLoading, isFetching, refetch, isError } = useQuery<
+    ProductType[],
+    Error
+  >({
     queryKey: ["products", page],
     queryFn: () => getProducts(offset, limit),
-    placeholderData: (previousData) => previousData, // ✅ substitui keepPreviousData
+    placeholderData: (previousData) => previousData,
   });
 
   return {
-    ...productsQuery,
-    products: productsQuery.data || [],
+    products: data || [],
+    error,
+    isLoading,
+    isFetching,
+    isError,
+    refetch,
   };
 }
